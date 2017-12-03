99d55b28155a9ecc3e2f669c022b2b810959c684
lyrric <xx20510@163.com>
2017-12-03 19:06:13 +0800

    啥也没有

--- user-api/src/main/java/com/play001/cloud/userapi/mapper/UserMapper.java
+++ user-api/src/main/java/com/play001/cloud/userapi/mapper/UserMapper.java
@@ -1,12 +1,21 @@
 package com.play001.cloud.userapi.mapper;
 
 import com.play001.cloud.userapi.entity.User;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper{
 
-    @Select("select id, username, password, phone, email, created_time, updated_time from jd_user where username = #{username}")
+    @Select("select id, username, password, phone, email, created_time as createdTime, " +
+            "updated_time as updatedTime from jd_user where username = #{username}")
     User findByUsername(String username);
+
+    @Insert("insert into(username, password, phone, email, created_time, updated_time) values(" +
+            "#{username}, #{password}, #{phone}, #{email}, #{createdTime}, #{updatedTime})")
+    void insert(User user);
+
+    @Select("select count(id) where username = #{username} or phone = #{phone} or email = #{email}")
+    Integer countByUserInfo(User User);
 }
