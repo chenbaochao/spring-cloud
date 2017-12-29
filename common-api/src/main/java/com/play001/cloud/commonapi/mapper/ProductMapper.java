97dbf043b41d416bfbe8ee6023eb19ffee8a7855
lyrric <xx20510@163.com>
2017-12-29 16:55:21 +0800

    微服务相互调用

---
+++ common-api/src/main/java/com/play001/cloud/commonapi/mapper/ProductMapper.java
@@ -0,0 +1,19 @@
+package com.play001.cloud.commonapi.mapper;
+
+import com.play001.cloud.common.entity.Product;
+import com.play001.cloud.common.entity.Response;
+import com.play001.cloud.commonapi.mapper.fallback.DefaultFallbackFactory;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * 微服务之间相互的调用
+ */
+@FeignClient(name = "ZUUL", fallbackFactory = DefaultFallbackFactory.class)
+public interface ProductMapper {
+
+    @RequestMapping(value = "/product/getDetail", method = RequestMethod.GET)
+    Response<Product> findById(@RequestParam("id") Long id);
+}
