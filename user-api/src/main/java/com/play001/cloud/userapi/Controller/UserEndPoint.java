6a45e58cb2c8a40e500fdf46b61560bee1073ba4
lyrric <xx20510@163.com>
2017-12-29 17:00:18 +0800

    获取用户基本信息

--- user-api/src/main/java/com/play001/cloud/userapi/Controller/UserEndPoint.java
+++ user-api/src/main/java/com/play001/cloud/userapi/Controller/UserEndPoint.java
@@ -2,6 +2,8 @@ package com.play001.cloud.userapi.Controller;
 
 import com.play001.cloud.common.entity.IException;
 import com.play001.cloud.common.entity.User;
+import com.play001.cloud.common.entity.UserCredential;
+import com.play001.cloud.common.util.JwtUtil;
 import com.play001.cloud.userapi.service.UserService;
 import com.play001.cloud.common.entity.Response;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +18,26 @@ public class UserEndPoint {
 
     @Autowired
     private UserService userService;
+
+    /**
+     * 登陆成功,返回令牌
+     * @param key 用户名or邮箱or电话号码
+     * @param password 密码
+     * @param expiryDate 令牌有效时长
+     * @return
+     */
+    @RequestMapping(value = "/getCredential", method = RequestMethod.GET)
+    public Response<String> login(String key, String password, Long expiryDate){
+        Response<String> response = new Response();
+        try {
+            response.setStatus(Response.SUCCESS);
+            response.setMessage(userService.getCredential(key, password, expiryDate));
+        }catch (Exception e){
+            response.setStatus(Response.ERROR);
+            response.setErrMsg(e.getMessage());
+        }
+        return response;
+    }
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
     public Response register(@Validated @RequestBody User user, BindingResult errResult, String code, @RequestHeader String registerCookie) throws Exception {
@@ -34,8 +56,14 @@ public class UserEndPoint {
         return userService.createCaptcha();
     }
 
-    @RequestMapping(value = "/test", method = RequestMethod.GET)
-    public void test() throws Exception {
-        throw new IException("出错啦");
+    /**
+     * 获取用户信息
+     * 只能获取自己的信息
+     * @throws Exception
+     */
+    @RequestMapping(value = "/getInfo", method = RequestMethod.GET)
+    public Response<User> getInfo(@RequestHeader("userJwt")String userJwt) throws Exception {
+        UserCredential userCredential = JwtUtil.getCredentialByJwt(userJwt);
+        return new Response<>(userService.getInfo(userCredential.getUserId()));
     }
 }
