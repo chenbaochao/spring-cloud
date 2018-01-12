02da39e342c388631893c02eb30008812c785572
lyrric <xx20510@163.com>
2018-01-12 17:22:27 +0800

    启动时扫描依赖包,自动注入

--- cms-web/src/main/java/com/play001/cloud/cms/Application.java
+++ cms-web/src/main/java/com/play001/cloud/cms/Application.java
@@ -2,8 +2,11 @@ package com.play001.cloud.cms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.play001.cloud"})
 public class Application {
     public static void main(String []args){
         SpringApplication.run(Application.class, args);
