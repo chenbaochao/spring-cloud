eb2b9579d4e5cbf4c1b5e6dad13b67056971c62b
lyrric <xx20510@163.com>
2018-01-12 17:21:51 +0800

    登陆日志记录

---
+++ cms-web/src/main/java/com/play001/cloud/cms/entity/LoginLog.java
@@ -0,0 +1,50 @@
+package com.play001.cloud.cms.entity;
+
+//登陆日志
+public class LoginLog {
+    private Long id;
+    private Integer adminId;
+    private String ip;
+    private String location;
+    private String time;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(Integer adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
