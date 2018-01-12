a8136acba2bedac927b26e74989dcf3a72cbe9da
lyrric <xx20510@163.com>
2018-01-12 17:21:21 +0800

    图片你懂得

---
+++ cms-web/src/main/java/com/play001/cloud/cms/mapper/ImageMapper.java
@@ -0,0 +1,31 @@
+package com.play001.cloud.cms.mapper;
+
+import com.play001.cloud.common.entity.Image;
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface ImageMapper {
+
+    /**
+     * 添加
+     */
+    @Insert("insert into support_image_log(url, path, create_time, used, storage_name) value(#{url}, #{path}, #{createTime}, #{used}, #{storageName})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    void add(Image image);
+
+    /**
+     * 通过ID查找
+     */
+    @Select("select id, url, path, used, storage_name as storageName, create_time as createTime from support_image_log where id = #{id}")
+    Image findById(Long id);
+
+    /**
+     * 设置图片为已用
+     */
+    @Update("update support_image_log set used = 1 where id = #{id}")
+    void setUsed(Long id);
+
+
+}
