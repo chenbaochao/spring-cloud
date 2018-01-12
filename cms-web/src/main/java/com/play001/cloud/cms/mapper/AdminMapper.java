1146f0b5c44550720e43ddcdf4d26f7d74313960
lyrric <xx20510@163.com>
2018-01-12 17:21:35 +0800

    复杂啦,复杂啦

--- cms-web/src/main/java/com/play001/cloud/cms/mapper/AdminMapper.java
+++ cms-web/src/main/java/com/play001/cloud/cms/mapper/AdminMapper.java
@@ -2,23 +2,34 @@ package com.play001.cloud.cms.mapper;
 
 import com.play001.cloud.cms.entity.Admin;
 import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Repository;
 
+import javax.annotation.Generated;
 import java.util.List;
 import java.util.Map;
 
 
 @Mapper
+@Repository
 public interface AdminMapper {
 
+    /**
+     * 更新头像
+     */
+    @Select("update cms_admin set avatar = #{url} where id = #{adminId}")
+    void updateAvatar(@Param("url") String url, @Param("adminId") Integer adminId);
 
     @Results({
             @Result(column = "id", property = "id"),
             @Result(column = "real_name", property = "realName"),
             @Result(property="role",column="role_id",one=@One(select="com.play001.cloud.cms.mapper.RoleMapper.findById"))
     })
-    @Select("select id, username, password, real_name, role_id  from cms_admin where username = #{username} limit 1")
+    @Select("select id, username, password, real_name, role_id, status  from cms_admin where username = #{username} limit 1")
     Admin findByUsername(String username);
 
+    /**
+     * 资料分页
+     */
     @Results({
             @Result(column = "id", property = "id"),
             @Result(column = "real_name", property = "realName"),
@@ -28,31 +39,60 @@ public interface AdminMapper {
     @Select("select id, username, real_name, age, email, telephone, status, create_time, role_id  from cms_admin limit #{offset}, #{limit}")
     List<Admin> Pagination(@Param("offset") Integer offset, @Param("limit") Integer limit);
 
+    /**
+     * 更新
+     */
+    @Update("update cms_admin set real_name = #{realName}, email = #{email}, telephone = #{telephone}, sex = #{sex}, status = #{status}, role_id = #{role.id} " +
+            " where id = #{id}")
+    void update(Admin admin);
+
+
 
     /**
-     * 获取用户操作权限
+     * 插入
      */
-    @Select("select m.menu_code as menuCode, am.flag from cms_admin_menu am, cms_menu m where m.id = am.menu_id and am.admin_id = #{id}")
-    List<Map<String, Object>> getMenuPermission(Integer id);
-
-    @Select("insert into cms_admin(username, role_id, password, real_name, email, status, telephone, sex, creator_id) value(" +
+    @Insert("insert into cms_admin(username, role_id, password, real_name, email, status, telephone, sex, creator_id) value(" +
             " #{username},#{role.id}, #{password}, #{realName}, #{email}, #{status}, #{telephone}, #{sex}, #{creator.id})")
-    void add(Admin admin);
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    Integer add(Admin admin);
 
-    //删除用户基本信息
+    /**
+     * 删除用户基本信息
+     */
+
     @Delete("delete from cms_admin where id = #{id}")
     void delete(Integer id);
-    //删除权限表
-    @Delete("delete from cms_admin_menu where admin_id = #{id}")
-    void deletePermission(Integer id);
 
+
+    /**
+     * findById
+     */
     @Results({
             @Result(column = "id", property = "id"),
             @Result(column = "real_name", property = "realName"),
+            @Result(column = "create_time", property = "createTime"),
             @Result(property="role",column="role_id",one=@One(select="com.play001.cloud.cms.mapper.RoleMapper.findById"))
     })
-    @Select("select id, username, password, real_name, email, telephone, age, sex, status, role_id  from cms_admin where id = #{id} limit 1")
+    @Select("select id, username, password, avatar, real_name, email, telephone, age, sex, status, role_id, create_time  from cms_admin where id = #{id} limit 1")
     Admin findById(Integer id);
 
+    /**
+     * 管理员列表 分页
+     */
+    @Select("select count(id) from cms_admin")
+    Integer count();
+
+    /**
+     * 设置用户状态
+     * @param status 冻结/启用= 1/0
+     */
+    @Update("update cms_admin set status = #{status} where id = #{id}")
+    void setStatus(@Param("id") Integer id, @Param("status") byte status);
+
+    /**
+     * 修改密码
+     */
+    @Update("update cms_admin set password = #{password} where id = #{id}")
+    void updatePassword(@Param("id") Integer id, @Param("password") String password);
 
 }
